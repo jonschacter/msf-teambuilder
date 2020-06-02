b45b01c2eceb2d42a59a18a914461de010d37070
github username <github email address>
2020-06-02 15:07:00 -0700

    add API index for teams

--- msf-teambuilder-backend/app/controllers/teams_controller.rb
+++ msf-teambuilder-backend/app/controllers/teams_controller.rb
@@ -1,2 +1,10 @@
 class TeamsController < ApplicationController
+    def index
+        teams = Team.all
+        render json: teams.as_json(:include => {
+            :characters => {
+                :except => [:team_id, :created_at, :updated_at]
+            }
+        }, :except => [:created_at, :updated_at])
+    end
 end
