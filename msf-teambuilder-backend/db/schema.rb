a10b52ea1ec6b664e3a893edb7cbe00d2e975bbe
github username <github email address>
2020-06-03 13:33:39 -0700

    add position column to characters table

--- msf-teambuilder-backend/db/schema.rb
+++ msf-teambuilder-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_210654) do
+ActiveRecord::Schema.define(version: 2020_06_03_203302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_210654) do
     t.bigint "team_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "position"
     t.index ["team_id"], name: "index_characters_on_team_id"
   end
 
